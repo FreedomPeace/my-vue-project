dd3ee7a9d679b8156bbbf6c46c5189c7909af804
FreedomPeace <18658251543@163.com>
2019-07-25 16:30:24 +0800

    去除多余的margin ，padding

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,35 @@
 <template>
   <div id="app">
-    <div class="header">
-      <mt-navbar
-        v-model="selected"
-        style="background-color: #fff0;padding-top: 15px">
+    <div style="height: 100%;">
+      <div class="header">
+        <mt-navbar
+          v-model="selected"
+          style="background-color: #fff0;padding-top: 15px">
 
-        <mt-tab-item id="1">
-          <img slot="icon" src="./assets/nom_apply.png"/>
-          基本信息
-        </mt-tab-item>
+          <mt-tab-item id="1">
+            <img slot="icon" src="./assets/nom_apply.png"/>
+            基本信息
+          </mt-tab-item>
 
-        <mt-tab-item id="2">
-          <img slot="icon" src="./assets/person.png"/>
-          个人信息
-        </mt-tab-item>
+          <mt-tab-item id="2">
+            <img slot="icon" src="./assets/person.png"/>
+            个人信息
+          </mt-tab-item>
 
-        <mt-tab-item id="3">
-          <img slot="icon" src="./assets/mail.png"/>
-          邮寄地址
-        </mt-tab-item>
+          <mt-tab-item id="3">
+            <img slot="icon" src="./assets/mail.png"/>
+            主页
+          </mt-tab-item>
 
-        <mt-tab-item id="4">
-          <img slot="icon" src="./assets/picture.png"/>
-          附件拍摄
-        </mt-tab-item>
+          <mt-tab-item id="4">
+            <img slot="icon" src="./assets/picture.png"/>
+            附件拍摄
+          </mt-tab-item>
 
-      </mt-navbar>
+        </mt-navbar>
+      </div>
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
@@ -67,14 +69,12 @@
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
   }
   .header {
     background: -webkit-gradient(linear, left top, right top, from(#6d99ff), to(#3c6cde));
     height: 125px;
-    position: absolute;
+    position: relative;
     top: 0;
     width: 100%;
     z-index: 999;
@@ -109,6 +109,9 @@
   .mint-tab-item-label {
     margin-top: 20px;
     color: #ddd;
+  }
+  .mint-navbar .mint-tab-item {
+    z-index: 999;
   }
 
 
