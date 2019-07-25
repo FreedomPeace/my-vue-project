6f60029af522330bc697eaa779085f13f75456ab
FreedomPeace <18658251543@163.com>
2019-07-25 14:49:07 +0800

    在命令行，使用如下命令，进行安装 npm install mint-ui
    修改main.js文件，把mint-ui引用到项目。

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import mui from 'mint-ui';
 
 Vue.config.productionTip = false
+Vue.use(mui);
 
 /* eslint-disable no-new */
 new Vue({
